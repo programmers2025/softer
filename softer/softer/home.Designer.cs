10fa5522ccfb6ccc87be70b380f919bafc5e08dc
mwissam <mwissam201@example.com>
2025-01-07 15:38:31 +0300

    8

--- softer/softer/home.Designer.cs
+++ softer/softer/home.Designer.cs
@@ -785,7 +785,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 839);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1765, 215);
+            this.pictureBox1.Size = new System.Drawing.Size(1746, 215);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
